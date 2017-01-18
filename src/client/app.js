0395df67bb67456fba4fc6713b76eb610997daab
forrana <forrana@gmail.com>
2017-01-18 13:48:01 +0300

    File uploader, webworker starter * Add file uploader element and logic, * Add start of webworker on file upload event

--- src/client/app.js
+++ src/client/app.js
@@ -1,8 +1,32 @@
-document.write('welcome to my app');
-console.log('app loaded');
 import io from 'socket.io-client';
+import Worker from "worker-loader!./webWorker";
 
-var connectionOptions =  {
+let fileUploader = document.querySelector('#fileUploder');
+
+fileUploder.addEventListener('change', (file) => {
+    let fr = new FileReader();
+
+    fr.onload = function(e) {
+        let arr = e.target.result.split('\n');
+        arr = arr.map(value => +value);
+
+        var worker = new Worker;
+        console.log('start processing', new Date());
+
+        worker.postMessage({
+                                size: 10000,
+                                array: arr
+                            });
+
+        worker.onmessage = function(event) {
+            console.log('end processing', new Date());
+            console.log(event.data);
+        }
+    };
+    fr.readAsText(event.target.files[0]);
+})
+
+let connectionOptions =  {
             "force new connection" : true,
             "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
             "timeout" : 10000,                  //before connect_error and connect_timeout are emitted.
@@ -15,5 +39,7 @@ socket.on('connect', () => {
     socket.emit('random');
     console.info('socket was connected');
 });
-socket.on('event', data => console.log(data) );
+socket.on('event', data => {
+    // console.log(slidingWindow.nextFrame());
+});
 socket.on('disconnect', function(){});
