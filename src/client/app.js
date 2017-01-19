c35575e8c93632d91eeda65b7d28d3099fd93fd2
forrana <forrana@gmail.com>
2017-01-19 11:32:30 +0300

    Add visualisation of all needed steps into our app

--- src/client/app.js
+++ src/client/app.js
@@ -1,45 +1,65 @@
-import io from 'socket.io-client';
-import Worker from "worker-loader!./webWorker";
+import Worker from './web.worker';
+import styles from './styles/app.css';
 
-let fileUploader = document.querySelector('#fileUploder');
+let fileUploader = document.querySelector('#fileUploder'),
+    button = document.querySelector('.button'),
+    loader = document.querySelector('#loader'),
+    resultLink = document.querySelector('#resultLink'),
+    uploadedFile,
+    interval;
 
-fileUploder.addEventListener('change', (file) => {
+let processFile = () => {
     let fr = new FileReader();
+    button.removeEventListener('click', startListener);
+    button.innerHTML = '<span id="timer"></span>';
 
     fr.onload = function(e) {
-        let arr = e.target.result.split('\n');
-        arr = arr.map(value => +value);
+        let inputArray = e.target.result.split('\n'),
+            worker = new Worker,
+            startTime;
 
-        var worker = new Worker;
-        console.log('start processing', new Date());
+        inputArray = inputArray.map(value => +value);
+        console.info('start processing', new Date().getTime());
+        startTime = Date.now();
+
+        interval = setInterval(function() {
+            var elapsedTime = Date.now() - startTime;
+            document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(3);
+        }, 100);
 
         worker.postMessage({
-                                size: 10000,
-                                array: arr
+                                size: 1000,
+                                array: inputArray
                             });
 
         worker.onmessage = function(event) {
-            console.log('end processing', new Date());
-            console.log(event.data);
+            console.info('end processing', new Date().getTime());
+            clearInterval(interval);
+            loader.style.display = 'none';
+
+            resultLink.download = 'results.csv';
+            resultLink.href = window.URL.createObjectURL(event.data.file);
+
+            button.classList.remove('button_start');
+            button.classList.add('button_download');
+            button.classList.add('pulse');
         }
     };
-    fr.readAsText(event.target.files[0]);
+    fr.readAsText(uploadedFile);
+}
+
+let startListener = event => {
+    processFile();
+    button.classList.remove('button_start');
+    button.classList.add('processing');
+    loader.style.display = 'block';
+}
+
+fileUploder.addEventListener('change', event => {
+    button.classList.add('button_start');
+    button.innerHTML = 'Start';
+
+    button.addEventListener('click', startListener);
+
+    uploadedFile = event.target.files[0];
 })
-
-let connectionOptions =  {
-            "force new connection" : true,
-            "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
-            "timeout" : 10000,                  //before connect_error and connect_timeout are emitted.
-            "transports" : ["websocket"]
-        };
-
-var socket = io('http://localhost:3003', connectionOptions);
-
-socket.on('connect', () => {
-    socket.emit('random');
-    console.info('socket was connected');
-});
-socket.on('event', data => {
-    // console.log(slidingWindow.nextFrame());
-});
-socket.on('disconnect', function(){});
