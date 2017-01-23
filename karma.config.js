57dfea7f34a35df5c84f870688946381da87b167
forrana <forrana@gmail.com>
2017-01-24 02:43:10 +0300

    Change karma browser for tests from Chrome to PhantomJS

--- karma.config.js
+++ karma.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = function(config) {
     config.set({
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
         coverageReporter: {
             reporters: [
                 {
