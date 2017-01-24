c278665cdfab0a2419789293a7af229104d0a811
forrana <forrana@gmail.com>
2017-01-24 11:16:14 +0300

    Fix karma.config.js file

--- karma.config.js
+++ karma.config.js
@@ -67,8 +67,8 @@ module.exports = function(config) {
                 ]
             }
         }
-        if (process.env.TRAVIS) {
-            cfg.browsers = ['Chrome_travis_ci'];
-        }
     });
+    if (process.env.TRAVIS) {
+        cfg.browsers = ['Chrome_travis_ci'];
+    }
 };
